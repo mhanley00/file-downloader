c497b5dcedd284415aeff20b15c6a3e195c6dd49
Madeline Hanley <mehanley2@gmail.com>
2019-10-11 12:28:08 -0400

    Updates

---
+++ precache-manifest.87f4eced7eb461015686bd99e0124ec0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1258c19838ec85d987f059d1a999e025",
+    "url": "/maped/nces-side-concept-app/test-downloader/index.html"
+  },
+  {
+    "revision": "1ac212cb94ef8b9b8cd7",
+    "url": "/maped/nces-side-concept-app/test-downloader/static/css/main.ae23be05.chunk.css"
+  },
+  {
+    "revision": "75b4b9bc2bbe74478435",
+    "url": "/maped/nces-side-concept-app/test-downloader/static/js/2.5e67b9c0.chunk.js"
+  },
+  {
+    "revision": "1ac212cb94ef8b9b8cd7",
+    "url": "/maped/nces-side-concept-app/test-downloader/static/js/main.1c1fefc4.chunk.js"
+  },
+  {
+    "revision": "e15420fda28119f2ab15",
+    "url": "/maped/nces-side-concept-app/test-downloader/static/js/runtime-main.492e0238.js"
+  },
+  {
+    "revision": "c4e2d23466a1fa2d361aed4428bf3c68",
+    "url": "/maped/nces-side-concept-app/test-downloader/static/media/LA_County_101pts.c4e2d234.csv"
+  },
+  {
+    "revision": "b2bf51b99fbdf9b2eab52a7d83ecc2fc",
+    "url": "/maped/nces-side-concept-app/test-downloader/static/media/SIDE_5000_Random_Pts_ipr1317.b2bf51b9.csv"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/maped/nces-side-concept-app/test-downloader/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/file-downloader/precache-manifest.0a96a5a853c77bf8617157a5ad91f5ae.js"
+  "/maped/nces-side-concept-app/test-downloader/precache-manifest.87f4eced7eb461015686bd99e0124ec0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/file-downloader/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/maped/nces-side-concept-app/test-downloader/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
